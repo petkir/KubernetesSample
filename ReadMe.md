d1e1c1ff5362116f0c394ebbb94fd46516bbe4b5
petkir <peter@petkir.at>
2020-06-16 20:12:10 +0200

    Reduced Costs only 1 Replica :(

--- ReadMe.md
+++ ReadMe.md
@@ -1,11 +1,22 @@
 # Kubernetes Sample
 
-TODO TODO TODO
 
-Concept:
 
-CMS Public URL internal TO SQL Deployed 
+## Concept:
+* Run MS SQL Server in Kubernetes Cluster
+* Use Backend for SPA
+* Indentity Management in Kubernetes
+* CMS in .net Core 
+* React Application (FrontEnd)
+* Angular (AdminApp)
+* Backend Api'S with Hardbeat
 
+## Additional Links
+[Deploy a SQL Server container in Kubernetes (Azure)] (https://docs.microsoft.com/en-us/sql/linux/tutorial-sql-server-containers-kubernetes?view=sql-server-ver15)
+
+https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-container-ha-overview?view=sql-server-ver15
+
+https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-replication?view=sql-server-ver15
 
 ## Disclaimer
 
--- deploy-all-on-k8s.sh
+++ deploy-all-on-k8s.sh
@@ -1,45 +1,52 @@
 #! /usr/bin/env bash
-
+#https://docs.microsoft.com/en-us/azure/aks/kubernetes-walkthrough
+#az aks get-credentials --resource-group AKSPetkir --name AKSpetkir
 
 if [ "$1" = "" ]; then
     echo -e "Provide a name for the namespace"
     exit 1
 fi
+# kubectl create secret docker-register yoursecretname --docker-server=jason.azurecr.io/xxxx/test --docker-username={UserName} --docker-password={Password} --docker-email=team@domain.com
+
 
 cd k8s
 kubectl create namespace ag1
 echo -e "Deploying DB Secrets"
-kubectl create secret generic mssql --from-literal=SA_PASSWORD="SecureAdminPW10" –namespace ag1
+kubectl create secret generic mssql --from-literal=SA_PASSWORD="SecureAdminPW10"  -n ag1
 
 echo -e "Deploying Secrets App Insight"
-kubectl create -f secrets.yaml -n $1
+kubectl create -f secrets.yaml -n ag1
 
 echo -e "Deploying  Persistent Volume"
-kubectl apply -f PersistentVolume.yaml
-echo -e "Deploying  SQL operator"
-kubectl apply -f AG1/operator.yaml — namespace ag1
-kubectl get pod — selector=”app=mssql-operator” -n ag1
-echo -e "Deploying  SQL sqlserver"
-kubectl apply -f AG1/sqlserver.yaml -n ag1
+kubectl apply -f PersistentVolume.yaml -n ag1
+# echo -e "Deploying  SQL operator HA not support for SQL 2019"
+# kubectl apply -f AG1/operator.yaml -n ag1
+# kubectl get pod --selector="app=mssql-operator" -n ag1
+# echo -e "Deploying  SQL sqlserver"
+# kubectl apply -f AG1/sqlserver.yaml -n ag1
+# echo -e "Deploying  ag-services"
+# kubectl apply -f AG1/ag-services.yaml -n ag1
+# kubectl get services -n ag1 -o wide
+# echo -e "Deploying  SQL failover"
+# kubectl apply -f failover.yaml  — namespace ag1
+echo -e "Deploying  SQL 2019"
+kubectl apply -f mssql.yaml -n ag1
 
-kubectl get pods –n ag1 -o wide
-echo -e "Deploying  ag-services"
-kubectl apply -f AG1/ag-services.yaml -n ag1
-kubectl get services -n ag1 -o wide
+kubectl get pods -n ag1 -o wide
+
 
 kubectl get pod -n ag1
 
 kubectl get service -n ag1
-echo -e "Deploying  SQL failover"
-kubectl apply -f failover.yaml  — namespace ag1
+
 kubectl get pod -n ag1
 
 
 echo -e "Deploy all BackendServices sind auch extern erreichbar"
-kubectl create -f backend.yaml -n $1
+kubectl apply -f backend.yaml -n ag1
 
 echo -e "Deploy all Front Services"
-kubectl create -f frontend.yaml -n $1
+kubectl apply -f frontend.yaml -n ag1
 # helm Hock with user creation would be nice
 # sqlcmd -S <External IP Address> -U sa -P "SecureAdminPW10"
 
